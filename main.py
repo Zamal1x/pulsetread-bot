7ff80d31d9d6c776e8c898cd9e646cfac538b1cb
MD ZAMAL AHMED <113609020+Zamal1x@users.noreply.github.com>
2026-06-17 22:20:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-‎TOKEN = "8762907720:AAG_BkZvBdL7SLt2OpgWufWtubrqNnulGUg"
-‎import os
-‎TOKEN = os.getenv("8762907720:AAHBjaUN6svqHL7OEk8Hx-IxxrOfnlu8tdA")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("8762907720:AAG_BkZvBdL7SLt2OpgWufWtubrqNnulGUg")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖 PulseTread AI Bot is Online!")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
